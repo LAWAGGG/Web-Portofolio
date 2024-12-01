5bb8b66a81ebfa88058412ca2b287e656fcf0592
AFh <ahmadfagih.arrifai@gmail.com>
2024-12-01 16:13:23 +0700

    Add files via upload

--- Web Portofolioo/Main.js
+++ Web Portofolioo/Main.js
@@ -8,30 +8,48 @@ let Nama = document.getElementById('Nama');
 let Skill = document.getElementById('Skills');
 let project = document.getElementById('Projects');
 let contact = document.getElementById('contacts');
+let navBar = document.querySelector('.navBar')
 lightBtn.addEventListener('click',()=>{
     Nama.classList.toggle('Light')
     Skill.classList.toggle('Light')
     project.classList.toggle('Light')
     contact.classList.toggle('Light')
+    navBar.classList.toggle('Light')
+    lightBtn.classList.toggle('bi-sun')
 })
 
 // modal detail
 let modal = document.getElementById('modalDetail')
 let openModal = document.getElementById('openDetail')
 let closeModal = document.getElementById('closeDetail')
+let modalContent = document.getElementById('modalContent')
 openModal.addEventListener('click', () => {
-    modal.style.display = 'flex';
+  modal.style.display = 'flex';
+  modal.classList.remove('hide'); // Pastikan kelas animasi keluar dihapus
+  modalContent.classList.remove('hide');
 });
 
 closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
+closeWithAnimation()
 });
 
 window.addEventListener('click', (event) => {
   if(event.target == modal){
-    modal.style.display = 'none';
+   closeWithAnimation()
   }
 })
+
+function closeWithAnimation() {
+  modal.classList.add('hide'); // Tambahkan animasi keluar ke modal
+  modalContent.classList.add('hide');
+
+  // Tunggu hingga animasi selesai sebelum mengubah display
+  setTimeout(() => {
+    modal.style.display = 'none';
+    modal.classList.remove('hide');
+    modalContent.classList.remove('hide');
+  }, 500); // Durasi ini harus sesuai dengan durasi animasi CSS
+}
 
 // modal tanya
 let modalTanya = document.getElementById('modalAsk')
